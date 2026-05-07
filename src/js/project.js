b48d8fd99a4ca467b48895157400ea7b546a7e74
Metrefua <metrefuatessema@gmail.com>
2026-05-07 15:10:10 +0300

    EDIT some points of Project.js

--- src/js/project.js
+++ src/js/project.js
@@ -3,22 +3,31 @@ import createTodo from "./todo";
 export default function createProject(name = "Default") {
   let todos = [];
 
+  // Get Todos
   const getTodos = () => todos;
 
+  // Add Todo
   const addTodo = (todoData) => {
     const todo = createTodo(todoData);
     todos.push(todo);
     return todo;
   };
 
+  //Remove todo
   const removeTodo = (id) => {
-    todos = todos.filter(todo => todo.id !== id);
+    todos = todos.filter(
+      (todo) => String(todo.id) !== String(id)
+    );
   };
 
+  // Get todo by ID
   const getTodoById = (id) => {
-    return todos.find(todo => todo.id === id);
+    return todos.find(
+      (todo) => String(todo.id) === String(id)
+    );
   };
 
+  // Update todo
   const updateTodo = (id, newData) => {
     const todo = getTodoById(id);
     if (todo) {
@@ -32,6 +41,6 @@ export default function createProject(name = "Default") {
     addTodo,
     removeTodo,
     getTodoById,
-    updateTodo
+    updateTodo,
   };
 }
